dd2466d1c3f06dbf1b86ab027c35cc038126026a
jesperxu <jesperxu@tencent.com>
2025-11-03 14:30:17 +0800

    init commit ,error

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('angular-app-1');
+  constructor() {
+    const o = {
+      a: 1
+      b: 2,
+    }
+  }
 }
